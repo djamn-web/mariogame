4a233ca9048596bb10ce4af8cb45897777eed31a
djamn <david@jamnig.net>
2025-11-30 23:06:38 +0100

    updated default bullet values

--- src/game/assets/tiled/bullet.tsx
+++ src/game/assets/tiled/bullet.tsx
@@ -5,7 +5,7 @@
   <properties>
    <property name="bullet_setback_x" type="int" value="-1"/>
    <property name="bullet_setback_y" type="int" value="-1"/>
-   <property name="velocity_x" type="int" value="-200"/>
+   <property name="velocity_x" type="int" value="0"/>
    <property name="velocity_y" type="int" value="0"/>
   </properties>
  </tile>
